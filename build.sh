d1153166631996f7ea8aff1060d67a6325d6b180
Benjamin Porter <bporter@instructure.com>
2015-12-30 22:12:11 +0000

    Fix build failure due to missing environment vars
    Also consolidate duplicate functionality into a function
    
    Change-Id: I32f2cd916200a3c4bd5b2e4e1e6412af7b57d5e0

--- build.sh
+++ build.sh
@@ -4,14 +4,27 @@
 # This is useful for running on a continuous integration server
 # that has a complex environment
 
+AB_RUN_LIVE=0
+AB_PARTNER_ID='<empty>'
+AB_PARTNER_KEY='<empty>'
+
+docker_run ()
+{
+  sudo docker run \
+    -e "ACADEMIC_BENCHMARKS_RUN_LIVE=${AB_RUN_LIVE}" \
+    -e "ACADEMIC_BENCHMARKS_PARTNER_ID=${AB_PARTNER_ID}" \
+    -e "ACADEMIC_BENCHMARKS_PARTNER_KEY=${AB_PARTNER_KEY}" \
+    academic_benchmarks_specs bundle exec rspec spec
+}
+
 echo 'Building docker image academic_benchmarks_specs'
 sudo docker build -t academic_benchmarks_specs .
 
 echo 'Running the regular unit tests'
-sudo docker run academic_benchmarks_specs bundle exec rspec spec
+docker_run
 
 #
-# run the tests that require credentials
+# uncomment to run the tests that require credentials
 # this can be commented out if you don't have Academic Benchmark
 # API credentials, or if you don't want to wait for the slower tests
 #
@@ -19,9 +32,8 @@ sudo docker run academic_benchmarks_specs bundle exec rspec spec
 # ACADEMIC_BENCHMARKS_PARTNER_ID and ACADEMIC_BENCHMARKS_PARTNER_KEY
 # set with the respective Academic Benchmarks API credential
 #
-echo 'Running live API tests'
-sudo -E docker run \
-  -e 'ACADEMIC_BENCHMARKS_RUN_LIVE=1' \
-  -e 'ACADEMIC_BENCHMARKS_PARTNER_ID' \
-  -e 'ACADEMIC_BENCHMARKS_PARTNER_KEY' \
-  academic_benchmarks_specs bundle exec rspec spec
+#echo 'Running live API tests'
+#AB_RUN_LIVE=1
+#AB_PARTNER_ID="${ACADEMIC_BENCHMARKS_PARTNER_ID}"
+#AB_PARTNER_KEY="${ACADEMIC_BENCHMARKS_PARTNER_KEY}"
+#docker_run
